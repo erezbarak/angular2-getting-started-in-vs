fd29d8250ecaf3fe014646205e6bb2eca732b1d8
Ward Bell <wardbell@hotmail.com>
2015-09-24 15:06:23 -0700

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,17 @@
+﻿import {Component, View, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: `
+	<h1>{{name}}\'s First Angular 2 App</h1>
+	<input [(ng-model)]="name"></input>
+	`,
+    directives: [FORM_DIRECTIVES]
+})
+class AppComponent {
+    name = 'John Doe';
+}
+
+bootstrap(AppComponent);
